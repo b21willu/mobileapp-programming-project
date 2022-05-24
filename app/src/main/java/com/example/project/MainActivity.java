42f0426aff2ccb2d16c73563e7e1a8bc71b58c51
b21willu <Wilmer990302>
2022-05-24 12:43:22 +0200

    Added JSON_URL in MainActivity. Enabled internet Access in AndroidManifest.xml.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=b21willu";
     RecyclerView recyclerView;
     ArrayList<Arenas> arenasArrayList;
     RecyclerViewAdapter adapter;
@@ -22,7 +23,11 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        new JsonTask(this).execute(JSON_URL);
+
         recyclerView = findViewById(R.id.recycler_view);
+        arenasArrayList = new ArrayList<>();
+        adapter = new RecyclerViewAdapter(arenasArrayList);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
     }
