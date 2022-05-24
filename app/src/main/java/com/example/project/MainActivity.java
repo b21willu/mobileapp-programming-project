80779ae6407b37034c88841f80d1dc0f058fc9ae
b21willu <Wilmer990302>
2022-05-24 12:17:24 +0200

    Added member variable RecyclerView and ArrayList<Arenas>

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -4,8 +4,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<Arenas> arenasArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
